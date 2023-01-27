af0a1cd5c6202a1f12a7758c8d5c299ea364f6dc
kiashko777 <kiashko777@gmail.com>
2023-01-27 06:10:20 +0400

    feat:menu item create
    admin menu item,route and controller are created

---
+++ app/code/MageMastery/Blog/Controller/Adminhtml/Post/Index.php
@@ -0,0 +1,23 @@
+<?php
+
+    declare(strict_types=1);
+
+    namespace MageMastery\Blog\Controller\Adminhtml\Post;
+
+    use Magento\Backend\App\Action;
+    use Magento\Framework\App\Action\HttpGetActionInterface;
+    use Magento\Framework\Controller\ResultFactory;
+    use Magento\Framework\View\Result\Page;
+
+    class Index extends Action implements HttpGetActionInterface
+    {
+
+        public function execute(): Page
+        {
+            /** @var Page $resultPage */
+            $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+            $resultPage->setActiveMenu('MageMastery_Blog::post');
+            $resultPage->getConfig()->getTitle()->prepend(__('Manage Posts'));
+            return $resultPage;
+        }
+    }
--- pub/index.php
+++ pub/index.php
@@ -1,17 +1,17 @@
 <?php
-/**
- * Public alias for the application entry point
- *
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
+    /**
+     * Public alias for the application entry point
+     *
+     * Copyright © Magento, Inc. All rights reserved.
+     * See COPYING.txt for license details.
+     */
 
-use Magento\Framework\App\Bootstrap;
+    use Magento\Framework\App\Bootstrap;
 
-try {
-    require __DIR__ . '/../app/bootstrap.php';
-} catch (\Exception $e) {
-    echo <<<HTML
+    try {
+        require __DIR__ . '/../app/bootstrap.php';
+    } catch (\Exception $e) {
+        echo <<<HTML
 <div style="font:12px/1.35em arial, helvetica, sans-serif;">
     <div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;">
         <h3 style="margin:0;font-size:1.7em;font-weight:normal;text-transform:none;text-align:left;color:#2f2f2f;">
@@ -20,11 +20,33 @@ try {
     <p>{$e->getMessage()}</p>
 </div>
 HTML;
-    http_response_code(500);
-    exit(1);
-}
+        http_response_code(500);
+        exit(1);
+    }
 
-$bootstrap = Bootstrap::create(BP, $_SERVER);
-/** @var \Magento\Framework\App\Http $app */
-$app = $bootstrap->createApplication(\Magento\Framework\App\Http::class);
-$bootstrap->run($app);
+    $bootstrap = Bootstrap::create(BP, $_SERVER);
+    /** @var \Magento\Framework\App\Http $app */
+    $app = $bootstrap->createApplication(\Magento\Framework\App\Http::class);
+    $bootstrap->run($app);
+
+    /**
+     * Code to test CRUD
+     */
+
+    //    $om = $bootstrap->getObjectManager();
+    //
+    //    /** @var \MageMastery\Blog\Model\ResourceModel\Post $postResource */
+    //    $postResource = $om->get(\MageMastery\Blog\Model\ResourceModel\Post::class);
+    //
+    //    /** @var \MageMastery\Blog\Model\Post $post */
+    //    $post = $om->create(\MageMastery\Blog\Model\Post::class);
+    //
+    //    $post->setData([
+    //        'title' => 'Test title',
+    //        'meta_title' => 'Test meta',
+    //        'content' => 'Test content',
+    //        'meta_keywords' => 'Test keywords',
+    //        'meta_description' => 'Test description',
+    //    ]);
+    //    $postResource->save($post);
+    //    var_dump($post->getData());
